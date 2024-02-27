e6473ad5736c3a5fc18870ebf2a348865f5a5a32
Chris-Kan <kandler.christian@johak.at>
2024-02-27 14:37:21 +0100

    show table

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,9 +4,17 @@
     let promise = supabase.from("countries").select();
 </script>
 
+<h1>Aufgaben:</h1>
+<ul><li>anstatt eines JS-Objekts,
+    das auf der Seite dargestellt wird,
+    möchte ich eine Liste aller Länder</li>
+    <li>daisy ui loading icon</li>
+</ul>
 
 {#await promise}
-<div>... loading</div>
+<div>... loading
+    <span class="loading loading-spinner loading-lg"></span>
+</div>
 {:then result}
 <div>{JSON.stringify(result)}</div>
 {/await}
