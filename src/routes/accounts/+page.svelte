877489649d377391d89dff68c5dd5265e163b836
Chris-Kan <kandler.christian@johak.at>
2024-04-30 14:54:43 +0200

    Create +page.svelte

---
+++ src/routes/accounts/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    import {supabase} from '$lib/supabase';
+    import {onMount} from 'svelte';
+
+    let accountList = ['😎'];
+
+    async function getAccounts() {
+        let {data: accounts, error} = await supabase.from('accounts').select('*');
+        accountList = accounts;
+    }
+
+    onMount(()=>{getAccounts();})
+</script>
+
+<div>{JSON.stringify(accountList)}</div>
